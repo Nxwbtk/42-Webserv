5771dd24e71d99333c1623d16da75c5185faff96
Your Name <you@example.com>
2024-04-06 22:32:09 +0700

    Add parseConfig method to ConfigParser class

--- inc/ConfigParser.hpp
+++ inc/ConfigParser.hpp
@@ -3,13 +3,19 @@
 # define CONFIGPARSER_HPP
 
 # include <iostream>
+# include <vector>
+# include "Server.hpp"
+
+class Server;
 
 class ConfigParser {
+    private:
+        std::string _filename;
     public:
         ConfigParser();
         ConfigParser(const std::string& filename);
         ~ConfigParser();
 
-        void parseConfig();
+        void parseConfig(std::vector<Server> _server);
 };
 #endif
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -12,3 +12,9 @@ ConfigParser::ConfigParser(const std::string& filename) {
 ConfigParser::~ConfigParser() {
     // Destructor
 }
+
+void ConfigParser::parseConfig(std::vector<Server> _server) {
+    // Parse the configuration file
+    (void)_server;
+    // I need to implement this method
+}
